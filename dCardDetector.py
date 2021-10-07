2ac5cb03addd473a8f23fba4d8e8db42b3fe95e2
daskro410 <72729971+daskro410@users.noreply.github.com>
2021-10-07 15:42:04 +0200

    update

--- dCardDetector.py
+++ dCardDetector.py
@@ -2,8 +2,13 @@ import cv2
 import numpy as np
 import dCards
 
+## Camera settings
+IM_WIDTH = 1280
+IM_HEIGHT = 720 
+FRAME_RATE = 30
+
 # Choose which webcam to capture, 0 for default, 1 for external
-cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
 
 # Check if the webcam is opened correctly
 if not cap.isOpened():
@@ -19,13 +24,53 @@ while True:
     #Preprocessing the video input (from dCards.py)
     preProc = dCards.preprocess_image(frame)
 
+    # Find and sort the contours of all cards in the image (query cards)
+    cnts_sort, cnt_is_card = dCards.find_cards(preProc)
+
+    # If there are no contours, do nothing
+    if len(cnts_sort) != 0:
+
+        # Initialize a new "cards" list to assign the card objects.
+        # k indexes the newly made array of cards.
+        cards = []
+        k = 0
+
+        # For each contour detected:
+        for i in range(len(cnts_sort)):
+            if (cnt_is_card[i] == 1):
+
+                # Create a card object from the contour and append it to the list of cards.
+                # preprocess_card function takes the card contour and contour and
+                # determines the cards properties (corner points, etc). It generates a
+                # flattened 200x300 image of the card, and isolates the card's
+                # suit and rank from the image.
+                cards.append(dCards.preprocess_card(cnts_sort[i], frame))
+
+                # Find the best rank and suit match for the card.
+                #cards[k].best_rank_match,cards[k].best_suit_match,cards[k].rank_diff,cards[k].suit_diff = dCards.match_card(cards[k],train_ranks,train_suits)
+
+                # Draw center point and match result on the image.
+                image = dCards.draw_results(frame, cards[k])
+                k = k + 1
+        
+        # Draw card contours on image (have to do contours all at once or
+        # they do not show up properly for some reason)
+        if (len(cards) != 0):
+            temp_cnts = []
+            for i in range(len(cards)):
+                temp_cnts.append(cards[i].contour)
+            cv2.drawContours(image,temp_cnts, -1, (255,0,0), 2)
+    
+    warped = dCards.flattener(frame, 4, 1, 1)
+
     #Draw contours
-    contours =  cv2.findContours(preProc, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[-2]
-    for c in contours:
-        cv2.drawContours(frame, [c], -1, (255,0,0), 3)
+    #contours =  cv2.findContours(preProc, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[-2]
+    #for c in contours:
+    #    cv2.drawContours(frame, [c], -1, (255,0,0), 3)
 
     #Show video stream
     cv2.imshow('Input', frame)
+    cv2.imshow('flattener', warped)
 
     c = cv2.waitKey(1)
     if c == 27: #Press escape to exit
--- dCards.py
+++ dCards.py
@@ -1,6 +1,26 @@
 import numpy as np
 import cv2
 
+### Constants ###
+# Adaptive threshold levels
+BKG_THRESH = 60
+CARD_THRESH = 30
+
+# Width and height of card corner, where rank and suit are
+CORNER_WIDTH = 32
+CORNER_HEIGHT = 84
+
+# Dimensions of rank train images
+RANK_WIDTH = 70
+RANK_HEIGHT = 125
+
+# Dimensions of suit train images
+SUIT_WIDTH = 70
+SUIT_HEIGHT = 100
+
+RANK_DIFF_MAX = 2000
+SUIT_DIFF_MAX = 700
+
 CARD_MAX_AREA = 120000
 CARD_MIN_AREA = 25000
 
@@ -17,15 +37,62 @@ class Query_card:
         self.suit_img = [] # Thresholded, sized image of card's suit
         self.best_rank_match = "Unknown" # Best matched rank
         self.best_suit_match = "Unknown" # Best matched suit
-        #self.rank_diff = 0 # Difference between rank image and best matched train rank image
-        #self.suit_diff = 0 # Difference between suit image and best matched train suit image
+        self.rank_diff = 0 # Difference between rank image and best matched train rank image
+        self.suit_diff = 0 # Difference between suit image and best matched train suit image
+
+class Train_ranks:
+    """Structure to store information about train rank images."""
+
+    def __init__(self):
+        self.img = [] # Thresholded, sized rank image loaded from hard drive
+        self.name = "Placeholder"
+
+class Train_suits:
+    """Structure to store information about train suit images."""
+
+    def __init__(self):
+        self.img = [] # Thresholded, sized suit image loaded from hard drive
+        self.name = "Placeholder"
+
+def load_ranks(filepath):
+    """Loads rank images from directory specified by filepath. Stores
+    them in a list of Train_ranks objects."""
+
+    train_ranks = []
+    i = 0
+    
+    for Rank in ['Ace','Two','Three','Four','Five','Six','Seven',
+                 'Eight','Nine','Ten','Jack','Queen','King']:
+        train_ranks.append(Train_ranks())
+        train_ranks[i].name = Rank
+        filename = Rank + '.jpg'
+        train_ranks[i].img = cv2.imread(filepath+filename, cv2.IMREAD_GRAYSCALE)
+        i = i + 1
+
+    return train_ranks
+
+def load_suits(filepath):
+    """Loads suit images from directory specified by filepath. Stores
+    them in a list of Train_suits objects."""
+
+    train_suits = []
+    i = 0
+    
+    for Suit in ['Spades','Diamonds','Clubs','Hearts']:
+        train_suits.append(Train_suits())
+        train_suits[i].name = Suit
+        filename = Suit + '.jpg'
+        train_suits[i].img = cv2.imread(filepath+filename, cv2.IMREAD_GRAYSCALE)
+        i = i + 1
+
+    return train_suits
 
 def preprocess_image(image):
     """Returns a grayed, blurred, and adaptively thresholded camera image."""
 
-    gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray,(5,5),0)
-    ret, thresh_img = cv2.threshold(blur, 200, 255, cv2.THRESH_BINARY)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) #Converting the current frame to gray
+    blur = cv2.GaussianBlur(gray,(5,5),0) #Blurring the grey frame
+    retval, thresh_img = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY_INV)
 
     # The best threshold level depends on the ambient lighting conditions.
     # For bright lighting, a high threshold must be used to isolate the cards
@@ -43,9 +110,9 @@ def preprocess_image(image):
     #retval, thresh = cv2.threshold(blur,thresh_level,255,cv2.THRESH_BINARY)
 
     #Uses Otsu thresholding
-    retval, otsu = cv2.threshold(gray,125,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    #retval, otsu = cv2.threshold(gray,125,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
     
-    return otsu
+    return thresh_img
 
 def find_cards(thresh_image):
     """Finds all card-sized contours in a thresholded camera image.
@@ -53,7 +120,7 @@ def find_cards(thresh_image):
     from largest to smallest."""
 
     # Find contours and sort their indices by contour size
-    dummy,cnts,hier = cv2.findContours(thresh_image,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    cnts,hier = cv2.findContours(thresh_image,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     index_sort = sorted(range(len(cnts)), key=lambda i : cv2.contourArea(cnts[i]),reverse=True)
 
     # If there are no contours, do nothing
@@ -89,7 +156,7 @@ def find_cards(thresh_image):
 
     return cnts_sort, cnt_is_card
 
-'''def preprocess_card(contour, image):
+def preprocess_card(contour, image):
     """Uses contour to find information about the query card. Isolates rank
     and suit images from the card."""
 
@@ -133,7 +200,7 @@ def find_cards(thresh_image):
     Qsuit = query_thresh[186:336, 0:128]
 
     # Find rank contour and bounding rectangle, isolate and find largest contour
-    dummy, Qrank_cnts, hier = cv2.findContours(Qrank, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    Qrank_cnts, hier = cv2.findContours(Qrank, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     Qrank_cnts = sorted(Qrank_cnts, key=cv2.contourArea,reverse=True)
 
     # Find bounding rectangle for largest contour, use it to resize query rank
@@ -145,7 +212,7 @@ def find_cards(thresh_image):
         qCard.rank_img = Qrank_sized
 
     # Find suit contour and bounding rectangle, isolate and find largest contour
-    dummy, Qsuit_cnts, hier = cv2.findContours(Qsuit, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    Qsuit_cnts, hier = cv2.findContours(Qsuit, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     Qsuit_cnts = sorted(Qsuit_cnts, key=cv2.contourArea,reverse=True)
     
     # Find bounding rectangle for largest contour, use it to resize query suit
@@ -156,4 +223,104 @@ def find_cards(thresh_image):
         Qsuit_sized = cv2.resize(Qsuit_roi, (SUIT_WIDTH, SUIT_HEIGHT), 0, 0)
         qCard.suit_img = Qsuit_sized
 
-    return qCard'''
+    return qCard
+
+def draw_results(image, qCard):
+    """Draw the card name, center point, and contour on the camera image."""
+
+    x = qCard.center[0]
+    y = qCard.center[1]
+    cv2.circle(image,(x,y),5,(255,0,0),-1)
+
+    rank_name = qCard.best_rank_match
+    suit_name = qCard.best_suit_match
+
+    # Draw card name twice, so letters have black outline
+    #cv2.putText(image,(rank_name+' of'),(x-60,y-10),1,(0,0,0),3,cv2.LINE_AA)
+    
+    #cv2.putText(image,(rank_name+' of'),(x-60,y-10),1,(50,200,200),2,cv2.LINE_AA)
+
+    #cv2.putText(image,suit_name,(x-60,y+25),1,(0,0,0),3,cv2.LINE_AA)
+    #cv2.putText(image,suit_name,(x-60,y+25),1,(50,200,200),2,cv2.LINE_AA)
+
+    cv2.putText(image, 'hej', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1, cv2.LINE_AA)
+    
+    # Can draw difference value for troubleshooting purposes
+    # (commented out during normal operation)
+    #r_diff = str(qCard.rank_diff)
+    #s_diff = str(qCard.suit_diff)
+    #cv2.putText(image,r_diff,(x+20,y+30),font,0.5,(0,0,255),1,cv2.LINE_AA)
+    #cv2.putText(image,s_diff,(x+20,y+50),font,0.5,(0,0,255),1,cv2.LINE_AA)
+
+    return image
+
+def flattener(image, pts, w, h):
+    """Flattens an image of a card into a top-down 200x300 perspective.
+    Returns the flattened, re-sized, grayed image.
+    See www.pyimagesearch.com/2014/08/25/4-point-opencv-getperspective-transform-example/"""
+    temp_rect = np.zeros((4,2), dtype = "float32")
+    
+    s = np.sum(pts, axis = 2)
+
+    tl = pts[np.argmin(s)]
+    br = pts[np.argmax(s)]
+
+    diff = np.diff(pts, axis = -1)
+    tr = pts[np.argmin(diff)]
+    bl = pts[np.argmax(diff)]
+
+    # Need to create an array listing points in order of
+    # [top left, top right, bottom right, bottom left]
+    # before doing the perspective transform
+
+    if w <= 0.8*h: # If card is vertically oriented
+        temp_rect[0] = tl
+        temp_rect[1] = tr
+        temp_rect[2] = br
+        temp_rect[3] = bl
+
+    if w >= 1.2*h: # If card is horizontally oriented
+        temp_rect[0] = bl
+        temp_rect[1] = tl
+        temp_rect[2] = tr
+        temp_rect[3] = br
+
+    # If the card is 'diamond' oriented, a different algorithm
+    # has to be used to identify which point is top left, top right
+    # bottom left, and bottom right.
+    
+    if w > 0.8*h and w < 1.2*h: #If card is diamond oriented
+        # If furthest left point is higher than furthest right point,
+        # card is tilted to the left.
+        if pts[1][0][1] <= pts[3][0][1]:
+            # If card is titled to the left, approxPolyDP returns points
+            # in this order: top right, top left, bottom left, bottom right
+            temp_rect[0] = pts[1][0] # Top left
+            temp_rect[1] = pts[0][0] # Top right
+            temp_rect[2] = pts[3][0] # Bottom right
+            temp_rect[3] = pts[2][0] # Bottom left
+
+        # If furthest left point is lower than furthest right point,
+        # card is tilted to the right
+        if pts[1][0][1] > pts[3][0][1]:
+            # If card is titled to the right, approxPolyDP returns points
+            # in this order: top left, bottom left, bottom right, top right
+            temp_rect[0] = pts[0][0] # Top left
+            temp_rect[1] = pts[3][0] # Top right
+            temp_rect[2] = pts[2][0] # Bottom right
+            temp_rect[3] = pts[1][0] # Bottom left
+            
+        
+    maxWidth = 200
+    maxHeight = 300
+
+    # Create destination array, calculate perspective transform matrix,
+    # and warp card image
+    dst = np.array([[0,0],[maxWidth-1,0],[maxWidth-1,maxHeight-1],[0, maxHeight-1]], np.float32)
+    M = cv2.getPerspectiveTransform(temp_rect,dst)
+    warp = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+    warp = cv2.cvtColor(warp,cv2.COLOR_BGR2GRAY)
+
+        
+
+    return warp
