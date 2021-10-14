36ac919040ec63240f5f7e5142bdcc086d02605d
daskro410 <72729971+daskro410@users.noreply.github.com>
2021-10-14 13:33:44 +0200

    Update

--- webcamthreshhold.py
+++ webcamthreshhold.py
@@ -1,8 +1,12 @@
+#Import libaries
 import cv2
 import numpy as np
 
+#Import other classes
+import processImage
+
 # Choose which webcam to capture, 0 for default, 1 for external
-cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
 # Check if the webcam is opened correctly
 if not cap.isOpened():
@@ -15,16 +19,12 @@ while True:
     # Resizing the webcam display size
     frame = cv2.resize(frame, None, fx=1.5, fy=1.5, interpolation=cv2.INTER_AREA)
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #Converting the current frame to gray
-    blur = cv2.GaussianBlur(gray,(5,5),0) #Blurring the grey frame
-    ret, thresh_img = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY_INV)
+    #Preprocessing the video input (from dCards.py)
+    preProc = processImage.preprocess_image(frame)
 
-    #meanThresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 115, 1) #Applys adaptive threshold to the grayscale image
-    #gauss = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115, 1) #Applys adaptive threshold to the grayscale image
-    retval2,otsu = cv2.threshold(gray,125,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    #Draw contours
+    contours =  cv2.findContours(frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
 
-    contours =  cv2.findContours(thresh_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
     for c in contours:
         cv2.drawContours(frame, [c], -1, (255,0,0), 3)
     
