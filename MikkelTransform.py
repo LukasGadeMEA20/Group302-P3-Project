c55ab0a3cae048bd5e52a63e7040a9093cfe1b9d
Lukas Gade Ravnsborg <lravns20@student.aau.dk>
2021-11-08 14:18:24 +0100

    Made it crash less.

--- MikkelTransform.py
+++ MikkelTransform.py
@@ -1,11 +1,10 @@
-import copy
 import cv2
 import numpy as np
 import matplotlib as plt 
 np.set_printoptions(formatter={'float_kind':"{:0.2f}".format})
 # Choose which webcam to capture, 0 for default, 1 for external
 #image = cv2.imread('C:\\Users\\profe\\OneDrive\\Skrivebord\\Github\\Group302-P3-Project\\dImages\\testImg.png')
-cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
+cap = cv2.VideoCapture(2, cv2.CAP_DSHOW)
 
 # Check if the webcam is opened correctly
 if not cap.isOpened():
@@ -13,48 +12,52 @@ if not cap.isOpened():
 
 while True:
     # Capture frame by frame
-    frame = cap.read()
+    ret, frame = cap.read()
 
     # Resizing the webcam display size
     frame = cv2.resize(frame, None, fx=1.5, fy=1.5, interpolation=cv2.INTER_AREA)
 
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #Converting the current frame to gray
+    blur = cv2.GaussianBlur(gray,(5,5),0) #Blurring the grey frame
     ret, thresh_img = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY_INV)
 
-    contours =  cv2.findContours(thresh_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
-    for c in contours:
-        ##cv2.drawContours(frame, [c], -1, (255,0,0), 3)
-    
-        #Approximate contour as a rectangle
-        perimeter = cv2.arcLength(c, True)
-        approx = cv2.approxPolyDP(c, 0.05 * perimeter, True)
-        approx = np.squeeze(approx) #Removes redundant dimension
 
-       # drawing points
-        for point in approx:
-            x = point[0]
-            y = point[1]
-            cv2.circle(frame, (x, y), 3, (0, 255, 0), -1)
-            cv2.putText(frame,str(x),(x,y),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),1,cv2.LINE_AA)
+    #pts2 = np.float32([[0,0],[0,300],[300,300],[300,0]])
 
-        # drawing skewed rectangle
-        cv2.drawContours(frame, [approx], -1, (0, 255, 0))
-
-    pts2 = np.float32([[0,0],[0,300],[300,300],[300,0]])
-
-    M = cv2.getPerspectiveTransform(approx.astype(np.float32),pts2)
-    print(M)
-    dst = cv2.warpPerspective(frame,M,(240,336))
-    
-    # Show the processed webcam feed
-    cv2.imshow('Threshold frame', thresh_img)
-    cv2.imshow('Camera frame', frame)
-    cv2.imshow('Transformed frame', dst)
-
+    #M = cv2.getPerspectiveTransform(approx.astype(np.float32),pts2)
+    #print(M)
+    #dst = cv2.warpPerspective(frame,M,(300,300))
     c = cv2.waitKey(1)
     if c == 27: #Press escape to exit
         break
+    elif c == 32: # Makes it so it only does the contour code when spacebar is clicked
+        contours =  cv2.findContours(thresh_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+        for c in contours:
+            cv2.drawContours(frame, [c], -1, (255,0,0), 3)
+        
+            #Approximate contour as a rectangle
+            perimeter = cv2.arcLength(c, True)
+            approx = cv2.approxPolyDP(c, 0.05 * perimeter, True)
+            approx = np.squeeze(approx) #Removes redundant dimension
+
+            # drawing points
+            for point in approx:
+                x = point[0]
+                y = point[1]
+                cv2.circle(frame, (x, y), 3, (0, 255, 0), -1)
+                cv2.putText(frame,str(x),(x,y),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),1,cv2.LINE_AA)
+
+            # drawing skewed rectangle
+            cv2.drawContours(frame, [approx], -1, (0, 255, 0))
+
+    # Show the processed webcam feed
+    cv2.imshow('Threshold frame', thresh_img)
+    cv2.imshow('Camera frame', frame)
+    #cv2.imshow('Transformed frame', dst)
+
+    
+
 
 cap.release()
 cv2.destroyAllWindows()
