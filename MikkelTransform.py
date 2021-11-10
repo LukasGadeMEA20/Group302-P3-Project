8289cdf5c004f496598cf93dbad77bf5401d2a55
Lukas Gade Ravnsborg <lravns20@student.aau.dk>
2021-11-10 15:33:53 +0100

    Cleaned up the code  a bit
    Made everything into functions

--- MikkelTransform.py
+++ MikkelTransform.py
@@ -5,25 +5,43 @@ import copy
 np.set_printoptions(formatter={'float_kind':"{:0.2f}".format})
 # Choose which webcam to capture, 0 for default, 1 for external
 #image = cv2.imread('C:\\Users\\profe\\OneDrive\\Skrivebord\\Github\\Group302-P3-Project\\dImages\\testImg.png')
-cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
+def loadCamera():
+    cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
 
-# Check if the webcam is opened correctly
-if not cap.isOpened():
-    raise IOError("Cannot open webcam")
+    # Check if the webcam is opened correctly
+    if not cap.isOpened():
+        raise IOError("Cannot open webcam")
 
-while True:
+    return cap
+
+def setCameraSize(cap):
     # Capture frame by frame
     ret, frame = cap.read()
+    
+    # Capture frame by frame
+    return cv2.resize(frame, None, fx=1.5, fy=1.5, interpolation=cv2.INTER_AREA)
+    
+#while True:
+    # Capture frame by frame
+    #ret, frame = cap.read()
 
     # Resizing the webcam display size
-    frame = cv2.resize(frame, None, fx=1.5, fy=1.5, interpolation=cv2.INTER_AREA)
+    #frame = cv2.resize(frame, None, fx=1.5, fy=1.5, interpolation=cv2.INTER_AREA)
 
+def grayScale(frame):
     # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #Converting the current frame to gray
-    blur = cv2.GaussianBlur(gray,(5,5),0) #Blurring the grey frame
-    ret, thresh_img = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY_INV)
+    return cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #Converting the current frame to gray
 
-    contours =  cv2.findContours(thresh_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+def blurImage(gray):
+    return cv2.GaussianBlur(gray,(5,5),0) #Blurring the grey frame
+
+def threshholdImage(gray):
+    return cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY_INV)
+
+def findContours(thresh_img):
+    return cv2.findContours(thresh_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+
+def drawContours(contours, frame, copiedFrame):
     for c in contours:
         cv2.drawContours(frame, [c], -1, (255,0,0), 3)
     
@@ -33,50 +51,57 @@ while True:
         approx = np.squeeze(approx) #Removes redundant dimension
 
         # drawing points
-        frame2 = copy.copy(frame)
-    try:
-        #drawing points
-        for point in approx:
-            x = point[0]
-            y = point[1]
-            cv2.circle(frame2, (x, y), 3, (0, 255, 0), -1)
-            cv2.putText(frame2,str(x),(x,y),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),1,cv2.LINE_AA)
+        try:
+            #drawing points
+            for point in approx:
+                x = point[0]
+                y = point[1]
+                cv2.circle(copiedFrame, (x, y), 3, (0, 255, 0), -1)
+                cv2.putText(copiedFrame,str(x),(x,y),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),1,cv2.LINE_AA)
         
-        # drawing skewed rectangle
-        cv2.drawContours(frame2, [approx], -1, (0, 255, 0))
-        ##print("THIS IS CONTOURS",contours)
-        if len(approx) == 4:
-            pts2 = np.float32([[0,0],[0,400],[300,400],[300,0]])
-            M = cv2.getPerspectiveTransform(approx.astype(np.float32),pts2)
-            #print(M)
-            dst = cv2.warpPerspective(frame,M,(300,400))
-            cv2.imshow('Transformed frame', dst)
+            # drawing skewed rectangle
+            cv2.drawContours(copiedFrame, [approx], -1, (0, 255, 0))
+            ##print("THIS IS CONTOURS",contours)
+            if len(approx) == 4:
+                pts2 = np.float32([[0,0],[0,400],[300,400],[300,0]])
+                M = cv2.getPerspectiveTransform(approx.astype(np.float32),pts2)
+                #print(M)
+                dst = cv2.warpPerspective(frame,M,(300,400))
+                cv2.imshow('Transformed frame', dst)
 
-            #Crop the image to get the artwork
-            croppedImg = dst[50:220, 30:270]
-            #Show cropped image
-            cv2.imshow("Cropped image", croppedImg)
-    except:
+                #Crop the image to get the artwork
+                croppedImg = dst[50:220, 30:270]
+                #Show cropped image
+                cv2.imshow("Cropped image", croppedImg)
+        except:
             print("could not find points")
-
-
-
-    
-    # Show the processed webcam feed
-    cv2.imshow('Threshold frame', thresh_img)
-    cv2.imshow('Camera frame', frame2)
-
-    # Show the processed webcam feed
-    cv2.imshow('Threshold frame', thresh_img)
-    cv2.imshow('Camera frame', frame)
-    #cv2.imshow('Transformed frame', dst)
-
-    c = cv2.waitKey(1)
-    if c == 27: #Press escape to exit
-        break
     #elif c == 32: # Makes it so it only does the contour code when spacebar is clicked
         
         
+if __name__ == '__main__':
+    cap = loadCamera()
+    while True:
+        frame = setCameraSize(cap)
+        copiedFrame = copy.copy(frame)
+        gray = grayScale(frame)
+        ret, thresh_img = threshholdImage(gray)
+        contours = findContours(thresh_img)
 
-cap.release()
-cv2.destroyAllWindows()
+        
+        # Show the processed webcam feed
+        cv2.imshow('Threshold frame', thresh_img)
+        cv2.imshow('Camera frame', copiedFrame)
+
+        # Show the processed webcam feed
+        cv2.imshow('Threshold frame', thresh_img)
+        cv2.imshow('Camera frame', frame)
+        #cv2.imshow('Transformed frame', dst)
+
+        c = cv2.waitKey(1)
+        if c == 27: #Press escape to exit
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
