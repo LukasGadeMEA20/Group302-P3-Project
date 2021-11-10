822219fff7a8252b476211c9e27992630b4872ba
daskro410 <72729971+daskro410@users.noreply.github.com>
2021-11-10 14:20:29 +0100

    gade dddddddddddddddddddddddd

--- MikkelTransform.py
+++ MikkelTransform.py
@@ -22,41 +22,38 @@ while True:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #Converting the current frame to gray
     blur = cv2.GaussianBlur(gray,(5,5),0) #Blurring the grey frame
     ret, thresh_img = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY_INV)
-    #M = cv2.getPerspectiveTransform(approx.astype(np.float32),pts2)
-    #print(M)
-    #dst = cv2.warpPerspective(frame,M,(300,300))
-    c = cv2.waitKey(1)
-    if c == 27: #Press escape to exit
-        break
-    elif c == 32: # Makes it so it only does the contour code when spacebar is clicked
-        contours =  cv2.findContours(thresh_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
-        for c in contours:
-            cv2.drawContours(frame, [c], -1, (255,0,0), 3)
-        
-            #Approximate contour as a rectangle
-            perimeter = cv2.arcLength(c, True)
-            approx = cv2.approxPolyDP(c, 0.05 * perimeter, True)
-            approx = np.squeeze(approx) #Removes redundant dimension
 
-            # drawing points
-            frame2 = copy.copy(frame)
-        try:
-            #drawing points
-            for point in approx:
-                x = point[0]
-                y = point[1]
-                cv2.circle(frame2, (x, y), 3, (0, 255, 0), -1)
-                cv2.putText(frame2,str(x),(x,y),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),1,cv2.LINE_AA)
-            
-            # drawing skewed rectangle
-            cv2.drawContours(frame2, [approx], -1, (0, 255, 0))
-            ##print("THIS IS CONTOURS",contours)
-            if len(approx) == 4:
-                pts2 = np.float32([[0,0],[0,400],[300,400],[300,0]])
-        except:
-                print("could not find points")
+    contours =  cv2.findContours(thresh_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+    for c in contours:
+        cv2.drawContours(frame, [c], -1, (255,0,0), 3)
+    
+        #Approximate contour as a rectangle
+        perimeter = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.05 * perimeter, True)
+        approx = np.squeeze(approx) #Removes redundant dimension
+
+        # drawing points
+        frame2 = copy.copy(frame)
+    try:
+        #drawing points
+        for point in approx:
+            x = point[0]
+            y = point[1]
+            cv2.circle(frame2, (x, y), 3, (0, 255, 0), -1)
+            cv2.putText(frame2,str(x),(x,y),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),1,cv2.LINE_AA)
         
-        
+        # drawing skewed rectangle
+        cv2.drawContours(frame2, [approx], -1, (0, 255, 0))
+        ##print("THIS IS CONTOURS",contours)
+        if len(approx) == 4:
+            pts2 = np.float32([[0,0],[0,400],[300,400],[300,0]])
+    except:
+            print("could not find points")
+    M = cv2.getPerspectiveTransform(approx.astype(np.float32),pts2)
+    #print(M)
+    dst = cv2.warpPerspective(frame,M,(300,300))
+
+    
     # Show the processed webcam feed
     cv2.imshow('Threshold frame', thresh_img)
     cv2.imshow('Camera frame', frame2)
@@ -65,6 +62,13 @@ while True:
     cv2.imshow('Threshold frame', thresh_img)
     cv2.imshow('Camera frame', frame)
     #cv2.imshow('Transformed frame', dst)
+
+    c = cv2.waitKey(1)
+    if c == 27: #Press escape to exit
+        break
+    #elif c == 32: # Makes it so it only does the contour code when spacebar is clicked
+        
+        
 
 cap.release()
 cv2.destroyAllWindows()
