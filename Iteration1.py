da44741409db19823858b2c4dbf7050a69438f62
Lukas Gade Ravnsborg <lravns20@student.aau.dk>
2021-11-29 10:29:12 +0100

    Manually merged branches

--- Iteration1.py
+++ Iteration1.py
@@ -2,6 +2,13 @@ import cv2
 import numpy as np
 import matplotlib as plt
 import copy
+import os
+import pyautogui
+
+DATABASE_PATH = 'card_data_base'
+N_IMAGES = 6
+IMG_WIDTH = 240
+IMG_HEIGHT = 170
 
 np.set_printoptions(formatter={'float_kind':"{:0.2f}".format})
 # Choose which webcam to capture, 0 for default, 1 for external
@@ -22,6 +29,26 @@ def setCameraSize(cap):
     # Capture frame by frame
     return cv2.resize(frame, None, fx=1.5, fy=1.5, interpolation=cv2.INTER_AREA)
     
+def load_database(verbose: bool = False) -> np.ndarray:
+    """Load and preprocess the database of images"""
+    database = np.zeros((N_IMAGES, int(IMG_WIDTH * IMG_HEIGHT)))  # Container for database
+    if verbose:
+        fig_list = []
+        ax_list = []
+        for _ in range(N_IMAGES):
+            fig, ax = plt.subplots(1, 3)
+            fig_list.append(fig)
+            ax_list.append(ax)
+
+    for i in range(N_IMAGES):
+        img = cv2.imread(os.path.join(DATABASE_PATH, f'new_card{i}.jpg'))  # Read image in BGR (height, width, 3)
+        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # Convert image to grayscale (height, width)
+        img_vector = img_gray.flatten()  # Convert image to vector (height * width)
+        img_vector = img_vector / np.linalg.norm(img_vector)  # Normalize vector such that ||img_vector||_2 = 1
+        database[i, :] = img_vector  
+
+    return database
+
 #while True:
     # Capture frame by frame
     #ret, frame = cap.read()
@@ -42,7 +69,17 @@ def otsuThreshholdImage(gray):
 def findContours(thresh_img):
     return cv2.findContours(thresh_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
 
-def drawContours(contours, frame, copiedFrame):
+def compare(greyCrop,database):
+    for i in range(N_IMAGES):
+        img = cv2.imread(os.path.join(DATABASE_PATH, f'new_card{i}.png'))  # Read image in BGR (height, width, 3)
+        img_vector = greyCrop.flatten()  # Convert image to vector (height * width)
+        img_vector = img_vector / np.linalg.norm(img_vector)  # Normalize vector such that ||img_vector||_2 = 1
+        dot_prod = np.dot(database, img_vector)  # Compute dot product b = A*x
+        dot_prod = dot_prod / np.sum(dot_prod)  # Normalize dot product such that sum is 1
+        # Print results:
+        print(f'Input image had index: {i} OMP predicts index: {np.argmax(dot_prod)}, with "probability": {dot_prod[np.argmax(dot_prod)]}')
+
+def drawContours(contours, frame, copiedFrame, database):
     for c in contours:
         cv2.drawContours(copiedFrame, [c], -1, (255,0,0), 3)
     
@@ -80,6 +117,7 @@ def drawContours(contours, frame, copiedFrame):
                 greyCrop = grayScale(croppedImg)
                 #ret, threshCrop = otsuThreshholdImage(greyCrop)
                 cv2.imshow("Cropped grey", greyCrop)
+                compare(greyCrop, database)
         except:
             print(" ")
     #elif c == 32: # Makes it so it only does the contour code when spacebar is clicked
@@ -87,6 +125,7 @@ def drawContours(contours, frame, copiedFrame):
         
 if __name__ == '__main__':
     cap = loadCamera()
+    database = load_database()
     while True:
         frame = setCameraSize(cap)
         copiedFrame = copy.copy(frame)
@@ -94,6 +133,7 @@ if __name__ == '__main__':
         ret, thresh_img = threshholdImage(gray, 100)
         contours= findContours(thresh_img)
         drawContours(contours, frame, copiedFrame)
+        drawContours(contours, frame, copiedFrame, database)
         #contours = findContours(thresh_img)
         
         # Show the processed webcam feed
