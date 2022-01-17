281fbadfe5eb5c1da897f88947e46b479df91696
daskro410 <72729971+daskro410@users.noreply.github.com>
2022-01-17 14:32:29 +0100

    New cards

--- card_data_base/artworkDownloader/resizecardimages.py
+++ card_data_base/artworkDownloader/resizecardimages.py
@@ -2,7 +2,7 @@ import PIL
 import os
 from PIL import Image
 
-f = r'C:\Users\mail\OneDrive\Dokumenter\scryfallimgdownload'
+f = r'C:\Users\mail\OneDrive\Dokumenter\GitHub\Group302-P3-Project\card_data_base\artworkDownloader'
 for file in os.listdir(f):
     if file.endswith('.jpg'):
         f_img = f+"/"+file
